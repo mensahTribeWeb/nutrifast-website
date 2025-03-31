0d7b11f6fa2814546cbaebb9cd51efbab01ad69f
Nick Mensah(Nokore.dev) <ndm.acct85@gmail.com>
2025-03-31 09:19:59 -0500

    set up tailwind

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login/login-page.component';
+import { SignupPageComponent } from './pages/signup/signup-page.component';
+import { DashboardPageComponent } from './pages/dashboard/dashboard-page.component';
+import { ProfileSettingsPageComponent } from './pages/profile-settings/profile-settings-page.component';
+import { NotFoundPageComponent } from './pages/not-found/not-found-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'signup', component: SignupPageComponent },
+  { path: 'dashboard', component: DashboardPageComponent },
+  { path: 'profile-settings', component: ProfileSettingsPageComponent },
+  { path: '**', component: NotFoundPageComponent }, // wildcard 404
+];
