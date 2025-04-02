0e6fbd3c5698d4416218183e09ae3dc2d2d059f1
Nick Mensah(Nokore.dev) <ndm.acct85@gmail.com>
2025-04-01 20:00:14 -0500

    Fixes for production

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,7 +11,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirebaseApp(() => initializeApp(environment)),
     provideAuth(() => getAuth()),
   ],
 };
